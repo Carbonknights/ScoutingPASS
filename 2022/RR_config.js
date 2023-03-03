54c19807f1816d1dddbac3fcaedebcd53de64da9
Carbonknights <125770690+Carbonknights@users.noreply.github.com>
2023-03-02 16:40:50 -0800

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2022ilpe",
+      "defaultValue": "2022caav",
       "required": "true",
       "disabled": "true"
     },
